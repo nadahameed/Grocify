5faa8d2a5a93e973f67a90ab24894c7cd3323aa4
Chiebuka <155529084+ebuka2005@users.noreply.github.com>
2024-07-28 10:53:03 -0400

    Update firebase.js
    Updated code where I ommited the analytics features.

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-import { getAnalytics } from "firebase/analytics";
+//import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -17,7 +17,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+//const analytics = getAnalytics(app);
 
 // Initialize Firestore and Auth
 const db = getFirestore(app);
