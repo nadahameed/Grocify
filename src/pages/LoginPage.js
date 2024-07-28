c7841eebf3a00305bd0452afe36bbe0b1f971cc6
Chiebuka <155529084+ebuka2005@users.noreply.github.com>
2024-07-28 13:36:16 -0400

    Update LoginPage.js

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../firebase';
 import './AuthPage.css';
+
 
 function LoginPage() {
   const [email, setEmail] = useState('');
