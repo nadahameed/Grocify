02aae64eeced09921f73d5507c71ad58cbe5962d
ebuka2005 <chiebukaonyejesi@gmail.com>
2024-07-28 11:24:23 -0400

    Revert "added all the pages"
    This reverts commit 0bee2dc81ff338defcd2ff950abd27e10f572696.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import './App.css';
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import Dashboard from './pages/Dashboard';
-import LoginPage from './pages/LoginPage';
-import SignupPage from './pages/SignupPage';
 
 function App() {
   return (
-    <Router>
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/signup" element={<SignupPage />} />
-      <Route path="/login" element={<LoginPage />} />
-    </Routes>
-  </Router>
-);
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
